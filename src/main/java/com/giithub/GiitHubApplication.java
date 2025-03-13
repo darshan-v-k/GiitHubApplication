70b5e1ba8e15a6943858f539a970dc3774db6c74
Darshan <dvkrockybhai@gmail.com>
2025-03-13 09:34:51 +0530

    Initial commit

---
+++ src/main/java/com/giithub/GiitHubApplication.java
@@ -0,0 +1,14 @@
+package com.giithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiitHubApplication.class, args);
+		System.out.println("HIIII !!!");
+	}
+
+}
